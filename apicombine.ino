66c297521080eba7a489234b056d8f91eba17907
kittipan2206 <89054342+kittipan2206@users.noreply.github.com>
2022-05-02 18:09:47 +0700

    Create apicombine.ino

---
+++ apicombine.ino
@@ -0,0 +1,220 @@
+// Import required libraries
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+
+
+// Set your access point network credentials
+const char* ssid = "ESP32-Access-Point";
+const char* password = "123456789";
+String arrival = "0";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server1(80);
+
+// Your GPRS credentials (leave empty, if not needed)
+const char apn[]      = "internet"; // APN (example: internet.vodafone.pt) use https://wiki.apnchanger.org
+const char gprsUser[] = "true"; // GPRS User
+const char gprsPass[] = "true"; // GPRS Password
+
+// SIM card PIN (leave empty, if not defined)
+const char simPIN[]   = ""; 
+
+// Server details
+// The server variable can be just a domain name or it can have a subdomain. It depends on the service you are using
+const char server[] = "asia-southeast1-psas-318318.cloudfunctions.net"; // domain name: example.com, maker.ifttt.com, etc
+const char resource[] = "/api-smartbus/api/signage/749BC19272544EC6A9C7015609BF0673/arrival";         // resource path, for example: /post-data.php
+const int  port = 80;                             // server port number
+
+// Keep this API Key value to be compatible with the PHP code provided in the project page. 
+// If you change the apiKeyValue value, the PHP file /post-data.php also needs to have the same key 
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+// TTGO T-Call pins
+#define MODEM_RST            5
+#define MODEM_PWKEY          4
+#define MODEM_POWER_ON       23
+#define MODEM_TX             27
+#define MODEM_RX             26
+#define I2C_SDA              21
+#define I2C_SCL              22
+// BME280 pins
+#define I2C_SDA_2            18
+#define I2C_SCL_2            19
+
+// Set serial for debug console (to Serial Monitor, default speed 115200)
+#define SerialMon Serial
+// Set serial for AT commands (to SIM800 module)
+#define SerialAT Serial1
+
+// Configure TinyGSM library
+#define TINY_GSM_MODEM_SIM800      // Modem is SIM800
+#define TINY_GSM_RX_BUFFER   1024  // Set RX buffer to 1Kb
+
+// Define the serial console for debug prints, if needed
+//#define DUMP_AT_COMMANDS
+
+#include <Wire.h>
+#include <TinyGsmClient.h>
+#include <jsonlib.h>
+
+#ifdef DUMP_AT_COMMANDS
+  #include <StreamDebugger.h>
+  StreamDebugger debugger(SerialAT, SerialMon);
+  TinyGsm modem(debugger);
+#else
+  TinyGsm modem(SerialAT);
+#endif
+
+// include <Adafruit_Sensor.h>
+// include <Adafruit_BME280.h>
+
+// I2C for SIM800 (to keep it running when powered from battery)
+TwoWire I2CPower = TwoWire(0);
+
+// I2C for BME280 sensor
+//TwoWire I2CBME = TwoWire(1);
+//Adafruit_BME280 bme; 
+
+// TinyGSM Client for Internet connection
+TinyGsmClient client(modem);
+
+#define uS_TO_S_FACTOR 1000000     /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  36        /* Time ESP32 will go to sleep (in seconds) 3600 seconds = 1 hour */
+
+#define IP5306_ADDR          0x75
+#define IP5306_REG_SYS_CTL0  0x00
+
+bool setPowerBoostKeepOn(int en){
+  I2CPower.beginTransmission(IP5306_ADDR);
+  I2CPower.write(IP5306_REG_SYS_CTL0);
+  if (en) {
+    I2CPower.write(0x37); // Set bit1: 1 enable 0 disable boost keep on
+  } else {
+    I2CPower.write(0x35); // 0x37 is default reg value
+  }
+  return I2CPower.endTransmission() == 0;
+}
+
+void setup() {
+  // Set serial monitor debugging window baud rate to 115200
+  SerialMon.begin(115200);
+
+  // Start I2C communication
+  I2CPower.begin(I2C_SDA, I2C_SCL, 400000);
+//  I2CBME.begin(I2C_SDA_2, I2C_SCL_2, 400000);
+
+  // Keep power when running from battery
+  bool isOk = setPowerBoostKeepOn(1);
+  SerialMon.println(String("IP5306 KeepOn ") + (isOk ? "OK" : "FAIL"));
+
+  // Set modem reset, enable, power pins
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
+
+  // Set GSM module baud rate and UART pins
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(3000);
+
+  // Restart SIM800 module, it takes quite some time
+  // To skip it, call init() instead of restart()
+  SerialMon.println("Initializing modem...");
+  modem.restart();
+  // use modem.init() if you don't need the complete restart
+
+  // Unlock your SIM card with a PIN if needed
+  if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(simPIN);
+  }
+  
+
+
+  // Configure the wake up source as timer wake up  
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  Serial.println();
+  
+  // Setting the ESP as an access point
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  
+  
+  // Start server
+  server1.begin();
+}
+
+void loop() {
+  SerialMon.print("Connecting to APN: ");
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    SerialMon.println(" fail");
+  }
+  else {
+    SerialMon.println(" OK");
+    
+    SerialMon.print("Connecting to ");
+    SerialMon.print(server);
+    if (!client.connect(server, port)) {
+      SerialMon.println(" fail");
+    }
+    else {
+      SerialMon.println(" OK");
+    
+      client.print(String("GET ") + resource + " HTTP/1.0\r\n");
+      client.print(String("Host: ") + server + "\r\n");
+      client.print("Connection: close\r\n\r\n");
+  
+      // Wait for data to arrive
+      uint32_t start = millis();
+      while (client.connected() && !client.available() &&
+             millis() - start < 30000L) {
+        delay(100);
+      };
+  
+      // Read data
+      start          = millis();
+      char logo[1200] = {
+          '\0',
+      };
+      int read_chars = 0;
+      while (client.connected() && millis() - start < 10000L) {
+        while (client.available()) {
+          logo[read_chars]     = client.read();
+          logo[read_chars + 1] = '\0';
+          read_chars++;
+          start = millis();
+        }
+      }
+//      SerialMon.println(logo);
+      
+      String str = String(logo);
+      SerialMon.println(str);
+
+      arrival = jsonExtract(str, "arrival");
+      Serial.println(arrival);
+
+        server1.on("/arrival", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/plain", arrival.c_str());
+      });
+      delay(5000);
+      
+      DBG("#####  RECEIVED:", strlen(logo), "CHARACTERS");
+      client.stop();
+      SerialMon.println(F("Server disconnected"));
+      modem.gprsDisconnect();
+      SerialMon.println(F("GPRS disconnected"));
+    }
+  }
+  // Put ESP32 into deep sleep mode (with timer wake up)
+  esp_deep_sleep_start();
+  sleep(3000);
+}
